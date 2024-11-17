1f640b24dd04fda88c14cb6f42c5eda095443f99
Alexandre Costa <costinhacosta2004@gmail.com>
2024-11-17 20:52:29 +0000

    base is done

--- transfer.c
+++ transfer.c
@@ -78,6 +78,14 @@ void sendMessage(int sockfd, const char *message) {
     }
 }
 
+void readMessage(int sockfd, char *response, size_t size) {
+    bzero(response, size);
+    if (read(sockfd, response, size - 1) < 0) {
+        perror("Error reading from socket");
+        exit(EXIT_FAILURE);
+    }
+}
+
 void closeSocket(int sockfd) {
     if (close(sockfd) < 0) {
         perror("close()");
@@ -85,22 +93,90 @@ void closeSocket(int sockfd) {
     }
 }
 
+void parsePASVResponse(char *response, char *ip, int *port) { // CHAT GPT nao sei oq é isto
+    int h1, h2, h3, h4, p1, p2;
+    sscanf(response, "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)", &h1, &h2, &h3, &h4, &p1, &p2);
+    sprintf(ip, "%d.%d.%d.%d", h1, h2, h3, h4);
+    *port = p1 * 256 + p2;
+}
 
 int main(int argc, char *argv[]) {
 
     char protocol[BUFFER_SIZE], user[BUFFER_SIZE], password[BUFFER_SIZE], host[BUFFER_SIZE], path[BUFFER_SIZE];
+    char response[BUFFER_SIZE], passiveIp[BUFFER_SIZE];
+    int ip;
 
     if (argc != 2 || parseURL(argv[1], protocol, user, password, host, path) != 0) {
         printf(stderr, "Write in format: %s ftp://[<user>:<password>@]<host>/<url-path>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    if (getIp(host) != 0){
+    if ((ip = getIp(host)) != 0){
         printf("Error trying to get the IP address \n");
         exit(EXIT_FAILURE);
     }
     
-    int *sockfd;
-    
+    int control_sockfd;
+    struct sockaddr_in server_addr;
+    initServerAddress(&server_addr);
+    control_sockfd = createSocket();
+    connectToServer(control_sockfd, &server_addr);
 
+    // Login
+    char command[BUFFER_SIZE];
+    snprintf(command, BUFFER_SIZE, "USER %s\n", user);
+    sendMessage(control_sockfd, command);
+    readMessage(control_sockfd, response, BUFFER_SIZE);
+    printf("Response: %s\n", response);
+
+    snprintf(command, BUFFER_SIZE, "PASS %s\n", password);
+    sendMessage(control_sockfd, command);
+    readMessage(control_sockfd, response, BUFFER_SIZE);
+    printf("Response: %s\n", response);
+
+    // Enter Passive Mode
+    snprintf(command, BUFFER_SIZE, "PASV\n");
+    sendMessage(control_sockfd, command);
+    readMessage(control_sockfd, response, BUFFER_SIZE);
+    printf("Response: %s\n", response);
+
+    int passivePort;
+    parsePASVResponse(response, passiveIp, &passivePort);
+    
+    int data_sockfd = createSocket();
+    struct sockaddr_in data_addr;
+    initServerAddress(&data_addr);
+    data_addr.sin_port = htons(passivePort);
+    connectToServer(data_sockfd, &data_addr);
+
+    snprintf(command, BUFFER_SIZE, "RETR %s\n", path);
+    sendMessage(control_sockfd, command);
+    readMessage(control_sockfd, response, BUFFER_SIZE);
+    printf("Response: %s\n", response);
+
+    FILE *f = fopen("pipe.txt", "wb");
+    if (!f) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[BUFFER_SIZE];
+    int bytesRead;
+    while ((bytesRead = read(data_sockfd, buffer, BUFFER_SIZE)) > 0) {
+        fwrite(buffer, 1, bytesRead, f);
+    }
+
+    fclose(f);
+    closeSocket(data_sockfd);
+
+    // Close Control Connection
+    snprintf(command, BUFFER_SIZE, "QUIT\n");
+    sendMessage(control_sockfd, command);
+    readMessage(control_sockfd, response, BUFFER_SIZE);
+    printf("Response: %s\n", response);
+
+    closeSocket(control_sockfd);
+
+    printf("File downloaded successfully!\n");
+    return 0;
 }
